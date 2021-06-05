c3433cb17d5d263008055248522cf65d4adcd4f7
Protuber <ddd183@mail.ru>
2021-06-05 12:18:53 +0300

    new remark

--- main.c
+++ main.c
@@ -3,14 +3,8 @@
 int main() {
 	
 	std::cout << "hi from Denis" << std::endl;
-<<<<<<< HEAD
 	std::cout << "hi from Denis 2" << std::endl;
 	std::cout <<"big hi from Pavel)"<<std::endl;	//it's fine don't forget git checkout...
 
-
-=======
-    std::cout << "Hi from Pavel" << std::endl;
-	
->>>>>>> c901dc717919529c9b1558b3bee084d085ea200c
 	return 0;
 }
